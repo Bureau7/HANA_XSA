336c9a328dcb1e041e99221e251387c83c27013b
Bureau7 Developer <developers@bureau7.com>
2018-03-03 17:13:41 +0000

    Nodemailer

--- NodeJS/Express_Website/app.js
+++ NodeJS/Express_Website/app.js
@@ -13,10 +13,57 @@ app.set('view engine', 'pug');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/',function(req, res){
-     res.render('index');   
-});
+     res.render('index',{title: 'Welcome!'}); 
+    });
 
+app.get('/about',function(req, res){
+        res.render('about',{about_title: 'About our company.'}); 
+    });
+    
+app.get('/contacts',function(req, res){
+        res.render('contacts'); 
+    });
+
+app.post('/contacts/send', function(req, res){
+
+       var transporter = nodemailer.createTransport({
+
+        service: 'Gmail',
+        auth: {
+            user: req.body.username,
+            pass: req.body.password //this is to pass password and email from form to test
+        }
+
+       });
+
+       var mailOptions = {
+
+        from: 'Bureau7',
+        to: 'developers@bureau7.com',
+        subject: 'Website Submission',
+        text: 'You have a submission with the following details: Name: '+req.body.name+'Email: '+req.body.email+'Message: '+req.body.message,
+        html: '<p>You have a submission with the following details: </p><ul><li>Name: '+req.body.name+'</li><li>Email: '+req.body.email+'</li><li>Message: '+req.body.message+'</li></ul>'
+
+       };
+
+       transporter.sendMail(mailOptions, function(error, info){
+
+        if(error){
+
+            console.log('Error sending data:'+error);
+            res.redirect('/');
+        } else {
+            console.log('Message Sent: '+info.response);
+            res.redirect('/');
+
+        }
+
+
+       });
+    });
+    
 app.listen(port);
 console.log("Server listening on port %d", port);
--- README.md
+++ README.md
@@ -9,7 +9,8 @@ All copyrights are with their respective owners.
 --------------------------------------------
 ```
 1. SimpleServer is just a concept.
-2. Express Website utilises - Pug (former Jade) is unavailable in SAP's NPM repository, so the public repo should be switched on following guide over here:
+2. Express Website utilises Pug (former Jade) which is unavailable in SAP's NPM repository, 
+   so the public repo should be switched on following guide over here:
    https://bitbucket.org/alunde/mta_upstream_registry
    (for endpoint api - xs api https://<hostname>:3<instance_number>30)	
 
